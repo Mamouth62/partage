cdd1035d7c5be986e97f7fe4f08c7fdcef62920c
Matheo <matheo.lucas1204@gmail.com>
2024-04-02 16:10:30 +0200

    save1

---
+++ CybersecuDS1/src/formulaire.php
@@ -0,0 +1,19 @@
+<fieldset>
+<legend>Formulaire</legend>
+<form method="POST" action="index.php?page=recupInscription.php">
+<p>
+<label>Nom: </label>
+<input type="text" name="nom" required maxlength="30" minlength="1"/>
+</p>
+<p>
+<label>Email: </label>
+<input type="text" name="email">
+</p>
+<p>
+<label>Age: </label>
+<input type="text" name="age">
+</p>
+<p>
+<label>Commentaire: </label>
+<input type="text" name="commentaire">
+</p>
