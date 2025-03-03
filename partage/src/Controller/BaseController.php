3df7b9d970e064f90603aa439f7572e13acf40c7
Matheo <matheo.lucas1204@gmail.com>
2025-03-03 16:36:01 +0100

    chap 4

---
+++ partage/src/Controller/BaseController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class BaseController extends AbstractController
+{
+    #[Route('/', name: 'app_accueil')]
+    public function index(): Response
+    {
+        return $this->render('base/index.html.twig', [
+        ]);
+    }
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(Request $request, EntityManagerInterface $em): Response
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $contact->setDateEnvoi(new \Datetime());
+                $em->persist($contact);
+                $em->flush();
+                $this->addFlash('notice', 'Message envoyé');
+                return $this->redirectToRoute('app_contact');
+            }
+        }
+
+        return $this->render('base/contact.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
